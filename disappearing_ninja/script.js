9232ce50686e4442daa01c60fe524b906cbc2896
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-02 18:12:23 -0800

    final

---
+++ disappearing_ninja/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+        $('#image1').click(function(){
+            $('#image1').hide();
+        });
+        $('#image2').click(function(){
+            $("#image2").hide();
+        });
+        $('#image3').click(function(){
+            $('#image3').hide();
+        });
+        $('#image4').click(function(){
+            $('#image4').hide();
+        });
+        $('#image5').click(function(){
+            $('#image5').hide();
+        });
+        $('#image6').click(function(){
+            $('#image6').hide();
+        });
+        $('#image7').click(function(){
+            $('#image7').hide();
+        });
+        $('#image8').click(function(){
+            $('#image8').hide();
+        });
+        $("button").click(function(){
+            location.reload(true);
+        });
+        
+    
+    
+});
