547bf65bb33a9f3afe1c2ab9bace292f8ffed081
abbykahlergi <70241972+abbykahler@users.noreply.github.com>
2021-02-02 18:39:09 -0800

    final

---
+++ ninja_to_cat/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+   $('img.zero').click(function() {
+      $(this).attr("src", "img/cat0.png");
+   });
+   $('img.one').click(function() {
+      $(this).attr("src", "img/ninja1.png");
+   });
+   $('img.two').click(function() {
+      $(this).attr("src", "img/cat2.png");
+   });
+   $('img.three').click(function() {
+      $(this).attr("src", "img/ninja3.png");
+   });
+   $('img.four').click(function() {
+
+      $(this).attr("src", "img/cat4.png");
+   });
+
+});
